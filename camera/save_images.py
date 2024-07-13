001529c402e5c95d3aed9ea1932eec9e5c718caf
LimKwangyoung <kokoz22@naver.com>
2024-07-13 15:18:05 +0900

    add: save images

---
+++ camera/save_images.py
@@ -0,0 +1,43 @@
+import cv2
+import os
+
+os.makedirs('images', exist_ok=True)
+os.makedirs('images/1', exist_ok=True)
+os.makedirs('images/2', exist_ok=True)
+os.makedirs('images/3', exist_ok=True)
+os.makedirs('images/4', exist_ok=True)
+
+img1_cnt = img2_cnt = img3_cnt = img4_cnt = 0
+
+# 0: rear camera, 1: front camera
+cap = cv2.VideoCapture(1)
+
+if not cap.isOpened():
+    exit()
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    cv2.imshow('Camera', frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord('q'):
+        break
+    elif key == ord('1'):
+        cv2.imwrite(f'images/1/{img1_cnt}.png', frame)
+        img1_cnt += 1
+    elif key == ord('2'):
+        cv2.imwrite(f'images/2/{img2_cnt}.png', frame)
+        img2_cnt += 1
+    elif key == ord('3'):
+        cv2.imwrite(f'images/3/{img3_cnt}.png', frame)
+        img3_cnt += 1
+    elif key == ord('4'):
+        cv2.imwrite(f'images/4/{img4_cnt}.png', frame)
+        img4_cnt += 1
+
+cap.release()
+cv2.destroyAllWindows()
